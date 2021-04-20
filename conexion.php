7f9e694b135fd3099477bc95801a7b1bf8bad1ec
Claudio Castillo <claudioandrescastillos@gmail.com>
2021-04-19 23:30:53 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,14 +1,6 @@
 <?php
-/*
-$dbh = mysqli_connect('localhost', 'u327958295_crist', 'r3yCr1st0', 'u327958295_tiend');
-if ($dbh->connect_error) {
-    die('Connect Error (' . $dbh->connect_errno . ') ' . $dbh->connect_error);
-}
-*/
 
-/* localhost */
-
-$dbh = mysql_connect ("127.0.0.1", "u327958295_crist", "r3yCr1st0") or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db ("u327958295_tiend"); 
+$dbh = mysql_connect ("localhost", "db_name", "password") or die ('I cannot connect to the database because: ' . mysql_error());
+mysql_select_db ("db_name"); 
 
 ?>
